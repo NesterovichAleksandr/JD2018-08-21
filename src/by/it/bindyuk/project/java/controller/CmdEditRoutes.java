d9e6037abfed35d3535d102b65d12e7682afcbb9
Khmelov <375336849110@tut.by>
2018-11-02 10:45:30 +0300

    bindyk fix

--- src/by/it/bindyuk/project/java/controller/CmdEditRoutes.java
+++ src/by/it/bindyuk/project/java/controller/CmdEditRoutes.java
@@ -1,14 +1,13 @@
 package by.it.bindyuk.project.java.controller;
 
-import by.it.bindyuk.project.java.dao.beans.Ticket;
+import by.it.bindyuk.project.java.dao.beans.Route;
 import by.it.bindyuk.project.java.dao.beans.User;
 import by.it.bindyuk.project.java.dao.dao.Dao;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.Timestamp;
 
-class CmdEditTicket extends Cmd {
+class CmdEditRoutes extends Cmd {
     @Override
     Cmd execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         Dao dao = Dao.getDao();
@@ -18,22 +17,15 @@ class CmdEditTicket extends Cmd {
         }
         if (Form.isPost(req)) {
             long id = Long.valueOf(Form.getString(req, "id"));
-            String transport = Form.getString(req, "transport");
-            long routesIdFrom = Long.valueOf(Form.getString(req, "routes_idFrom"));
-            long routesIdTo = Long.valueOf(Form.getString(req, "routes_idTo"));
-            Timestamp data = Timestamp.valueOf(Form.getString(req, "data"));
-            long users_id = Long.valueOf(req.getParameter("users_id"));
-
-            Ticket ticket = new Ticket(id, transport, routesIdFrom, routesIdTo, data, users_id);
+            String city = Form.getString(req, "city");
+            Route route = new Route(id, city);
             if (req.getParameter("Update") != null) {
-                dao.ticket.update(ticket);
+                dao.route.update(route);
             }
             if (req.getParameter("Delete") != null) {
-                dao.ticket.delete(ticket);
+                dao.route.delete(route);
             }
         }
-        req.setAttribute("tickets", dao.ticket.getAll());
-        req.setAttribute("users", dao.user.getAll());
         req.setAttribute("routes", dao.route.getAll());
         return null;
     }
